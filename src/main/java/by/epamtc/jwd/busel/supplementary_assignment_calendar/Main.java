fbc40868eb82b05ff1e98fd210e9dbf7880a4419
Siarhei Busiel <sju.busel@gmail.com>
2020-07-23 20:32:04 +0300

    Attempt to create an adjustable calendar

--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment_calendar/Main.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment_calendar/Main.java
@@ -3,21 +3,23 @@ package by.epamtc.jwd.busel.supplementary_assignment_calendar;
 import by.epamtc.jwd.busel.supplementary_assignment_calendar.util.MyCalendarFactory;
 import by.epamtc.jwd.busel.supplementary_assignment_calendar.util.MyCalendarPrinter;
 
-import java.util.*;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-        int month = 0;      // from 0 to 11 inclusively
-        int year = 2021;    // any year
+        int month = 6;      // from 0 to 11 inclusively
+        int year = 2020;    // any year
 
         MyCalendarFactory calendarFactory = MyCalendarFactory.getInstance();
         Calendar cal = calendarFactory.getCalendar(year, month);
 
         MyCalendarPrinter printer = new MyCalendarPrinter();
         printer.printActualMonth(cal);
-
         System.out.println();
+        printer.printActualYearOneAfterAnother(cal);
+        System.out.println();
+        printer.printActualYearInColumns(cal, 2);
 
-        printer.printActualYear(cal);
+
     }
 }
--- src/main/java/by/epamtc/jwd/busel/supplementary_assignment_calendar/util/MyCalendarPrinter.java
+++ src/main/java/by/epamtc/jwd/busel/supplementary_assignment_calendar/util/MyCalendarPrinter.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MyCalendarPrinter {
+    private static final int MONTHS_IN_YEAR = 12;
     private static final int DAYS_IN_WEEK = 7;
     private static final int SUNDAY_MASK = DAYS_IN_WEEK - 1;
     private static final int NOT_SUNDAY_MASK = DAYS_IN_WEEK - 2;
     private static final String DAY_DELIMITER = "  ";
+    private static final String MONTH_DELIMITER = DAY_DELIMITER + DAY_DELIMITER;
     private static final int CELL_LENGTH = DAY_DELIMITER.length() + 2;
     private static final String CELL_FILLER = " ";
     private static final int LINE_LENGTH = DAYS_IN_WEEK * CELL_LENGTH;
@@ -37,6 +39,51 @@ public class MyCalendarPrinter {
         MONTHS_NAMES.putIfAbsent(Calendar.DECEMBER, "Снежань");
     }
 
+    public void printActualMonth(Calendar cal) {
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        int actualMaximumDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int actualMinimumDay = cal.getActualMinimum(Calendar.DAY_OF_MONTH);
+
+        StringBuilder sb = new StringBuilder();
+        generateAndAddHeader(sb, cal);
+
+        String offset = calculateFirstDayOfMonthOffset(dayOfWeek);
+        sb.append(offset);
+
+        generateAndAddMonthBody(sb, dayOfWeek, actualMinimumDay, actualMaximumDay);
+
+        System.out.println(new String(sb));
+
+    }
+
+    public void printActualYearOneAfterAnother(Calendar cal) {
+        for (int i = 0; i < Calendar.UNDECIMBER; i++) {
+            cal.set(Calendar.MONTH, i);
+            printActualMonth(cal);
+        }
+    }
+
+    public void printActualYearInColumns(Calendar cal, int columnQuantity) {
+        int linesOfMonthsNumber = (MONTHS_IN_YEAR % columnQuantity) != 0
+                                  ? MONTHS_IN_YEAR / columnQuantity + 1
+                                  : MONTHS_IN_YEAR / columnQuantity;
+
+        for (int i = 0; i < linesOfMonthsNumber; i++) {
+            int diff = MONTHS_IN_YEAR - (i * columnQuantity);
+            if (diff > linesOfMonthsNumber) {
+                printLineOfMonths(cal, i * columnQuantity, (i + 1) * columnQuantity);
+            }
+            else {
+                printLineOfMonths(cal, i * columnQuantity, MONTHS_IN_YEAR);
+            }
+        }
+
+    }
+
+//////////////////////////////////////////////////
+// #printActualMonth(Calendar) computation methods
+//////////////////////////////////////////////////
+
     private static String calculateFirstDayOfMonthOffset(int dayOfWeek) {
         int diff = DAYS_IN_WEEK - dayOfWeek;
         int offsetMultiplicator;
@@ -64,7 +111,7 @@ public class MyCalendarPrinter {
     }
 
     private void generateAndAddMonthBody(StringBuilder sb, int dayOfWeek,
-            int actualMaximumDay, int actualMinimumDay) {
+            int actualMinimumDay, int actualMaximumDay) {
         for (int i = actualMinimumDay; i <= actualMaximumDay; i++) {
             sb.append(DAY_DELIMITER);
             sb.append(String.format("%2d", i));
@@ -77,27 +124,51 @@ public class MyCalendarPrinter {
         }
     }
 
-    public void printActualMonth(Calendar cal) {
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        int actualMaximumDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        int actualMinimumDay = cal.getActualMinimum(Calendar.DAY_OF_MONTH);
+///////////////////////////////////////////////////////////////
+// #printActualYearInColumns(Calendar, int) computation methods
+///////////////////////////////////////////////////////////////
 
+    private void printLineOfMonths(Calendar cal, int minMonth, int maxMonth) {
         StringBuilder sb = new StringBuilder();
-        generateAndAddHeader(sb, cal);
+        int weeksNumber = getMaxWeeksNumber(cal, minMonth, maxMonth);
+        // generate 1st line "MONTH_NAME & YEAR"
+        String delimiter = "";
+        for (int j = minMonth; j < maxMonth; j++) {
+            cal.set(Calendar.MONTH, j);
+            sb.append(delimiter);
+            String month = MONTHS_NAMES.get(cal.get(Calendar.MONTH));
+            int year = cal.get(Calendar.YEAR);
+            String header = String.format("%" + LINE_LENGTH + "s",
+                    month + CELL_FILLER + year);
+            sb.append(header);
+            delimiter = MONTH_DELIMITER;
+        }
+        sb.append('\n');
 
-        String offset = calculateFirstDayOfMonthOffset(dayOfWeek);
-        sb.append(offset);
-
-        generateAndAddMonthBody(sb, dayOfWeek, actualMinimumDay, actualMaximumDay);
-
-        System.out.println(new String(sb));
-
+        // generate 2nd line "MONTH_HEADER"
+        delimiter = "";
+        for (int i = minMonth; i < maxMonth; i++) {
+            cal.set(Calendar.MONTH, i);
+            sb.append(delimiter);
+            sb.append(MONTH_HEADER);
+            delimiter = MONTH_DELIMITER;
+        }
+        sb.append('\n');
+        System.out.println(sb.toString());
+//        for (int i = 0; i < weeksNumber; i++) {
+//
+//        }
     }
 
-    public void printActualYear(Calendar cal) {
-        for (int i = 0; i < Calendar.UNDECIMBER; i++) {
-            cal.set(Calendar.MONTH, i);
-            printActualMonth(cal);
+    private int getMaxWeeksNumber(Calendar cal, int minMonth, int maxMonth) {
+        int maxWeeksNumber = Integer.MIN_VALUE;
+        for (int i = minMonth; i < maxMonth; i++) {
+            cal.set(Calendar.MONTH, minMonth);
+            int weeksNumber = cal.get(Calendar.WEEK_OF_MONTH);
+            if (weeksNumber > maxWeeksNumber) {
+                maxWeeksNumber = weeksNumber;
+            }
         }
+        return maxWeeksNumber;
     }
 }
